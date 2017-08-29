07c6408bb2e9ae9d7d57aed52fa9d66ceea14a52
Oliver Blum <oblum@compvisgpu01>
2017-08-29 11:48:51 +0200

    Added Info Gan loss

--- ld_gan/models/dis.py
+++ ld_gan/models/dis.py
@@ -1,43 +1,8 @@
 import torch.nn as nn
 from init_weights import init_weights
 import numpy as np
-
-
-
-class dis_64(nn.Module):
-    
-    def __init__(self, 
-                 complexity = 64,
-                 n_col = 3):
-        
-        super(dis_64, self).__init__()
-        self.main = nn.Sequential(
-            # BLOCK 0: 64 --> 32
-            nn.Conv2d(n_col, complexity, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # BLOCK 1: 32 --> 16
-            nn.Conv2d(complexity, complexity * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(complexity * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            # BLOCK 2: 16 --> 8
-            nn.Conv2d(complexity * 2, complexity * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(complexity * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            # BLOCK 3: 8 --> 4
-            nn.Conv2d(complexity * 4, complexity * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(complexity * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            # BLOCK 4: 4 --> 1
-            nn.Conv2d(complexity * 8, 1, 4, 1, 0, bias=False),
-            nn.Sigmoid()
-        )
-        self.main.apply(init_weights)
-        self.main.cuda()
-
-    def forward(self, input):
-        output = self.main(input)
-        return output
-    
+import torch.nn.functional as F
+import torch
     
     
 class Dis(nn.Module):
@@ -76,5 +41,53 @@ class Dis(nn.Module):
         return x
     
     
+    
+    
+class InfoDis(nn.Module):
+    
+    def __init__(self, 
+                 ipt_size   = 64,
+                 complexity = 64,
+                 n_col      = 3,
+                 n_features = 256):
+        
+        super(InfoDis, self).__init__()
+        
+        self.n_blocks = int(np.log2(64) - 1)
+        self.main = nn.Sequential()
+        
+        # BLOCK 0
+        self.main.add_module('b00', nn.Conv2d(n_col, complexity, 4, 2, 1, bias=False))
+        self.main.add_module('b01', nn.LeakyReLU(0.2, inplace=True))
+
+        # BLOCKS 1 - N-1
+        for b in range(1, self.n_blocks - 1):
+            c_in  = complexity * 2**(b-1)
+            c_out = complexity * 2**(b)
+            n = 'b' + str(b)
+            self.main.add_module(n+'0', nn.Conv2d(c_in, c_out, 4, 2, 1, bias=False))
+            self.main.add_module(n+'1', nn.BatchNorm2d(c_out))
+            self.main.add_module(n+'2', nn.LeakyReLU(0.2, inplace=True))
+        
+        # BLOCK N: 4 --> 1
+        n = 'b' + str(self.n_blocks-1)
+        self.main.add_module(n+"0", nn.Conv2d(c_out, n_features, 4, 1, 0, bias=False))
+        
+        self.fc0 = nn.Linear(2*n_features, n_features)
+        self.bn0 = nn.BatchNorm1d(n_features)
+        self.fc1 = nn.Linear(n_features, 1)
+
+       
+    
+    def forward(self, x, z):
+        x = self.main(x)
+        x = torch.cat((x,z), 1)
+        x = x.view(-1, x.size(1))
+        x = self.fc0(x)
+        x = F.leaky_relu(x, 0.2, inplace=True)
+        x = self.bn0(x)
+        x = self.fc1(x)
+        x = F.sigmoid(x)
+        return x
     
     
--- ld_gan/train_ops/__init__.py
+++ ld_gan/train_ops/__init__.py
@@ -3,3 +3,5 @@ from gan_dis import GanDis
 from ae_enc import AutoEnc
 from clf_enc import Clf
 from floss_gen import FLoss
+from infogan_dis import InfoDis
+from infogan_gen import InfoGen
