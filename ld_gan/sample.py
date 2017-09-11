fix bug in nn_sampler (normalisation & making it mutch faster)

ld_gan/sample.py
@@ -286,10 +286,10 @@ def nn_sampler(z_enc, imgs, y, batch_size, n_neighbors = 5, n_jobs = 10):
         y_batch = y[batch_idxs]
         
         yield img_batch, y_batch, z_batch, z_batch
-        
-        
+
         
 # 10 life
+import torch.nn.functional as F
 def nn_sampler_life(enc, 
                     X, 
                     y, 
@@ -307,16 +307,15 @@ def nn_sampler_life(enc,
             imgs = X[rand_idxs]
         z_enc = ld_gan.utils.model_handler.apply_model(enc, imgs, batch_size = 1000)
         
-        dists, idxs = NearestNeighbors(n_neighbors=n_neighbors,
-                                       n_jobs=n_jobs).fit(z_enc).kneighbors(z_enc)
-        
         batch_idxs = np.random.randint(0, len(z_enc), batch_size)
         
+        dists, idxs = NearestNeighbors(n_neighbors=n_neighbors,
+                            n_jobs=n_jobs).fit(z_enc).kneighbors(z_enc[batch_idxs])
+
         # get z_batch
-        batch_z_idxs = idxs[batch_idxs]
-        batch_z_all = z_enc[batch_z_idxs]
+        batch_z_all = z_enc[idxs]
         rand_weights = np.random.rand(n_neighbors, batch_size)
-        rand_weighst = rand_weights / np.sum(rand_weights, axis=0)
+        rand_weights = rand_weights / np.sum(rand_weights, axis=0)
         rand_weights = rand_weights.transpose()
         z_batch = [np.average(z_all, 0, w) for w, z_all in zip(rand_weights, batch_z_all)]
         z_batch = np.array(z_batch)
@@ -326,9 +325,6 @@ def nn_sampler_life(enc,
         y_batch = y[batch_idxs]
         
         yield img_batch, y_batch, z_batch, z_batch
-        
-        
-        
         
         
 def find_ideal_kde_sampling_bandwidth(ipt, 

ld_gan/train_ops/triplet_enc.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 import torch.optim as optim
 import numpy as np
 from ld_gan.utils.model_handler import apply_model, apply_models
-from tripletnet import Tripletnet
+import torch.nn.functional as F
 
     
 class TripletEnc:
@@ -64,6 +64,19 @@ class TripletEnc:
         
         return err.cpu().data.numpy()[0]
     
+
+class Tripletnet(nn.Module):
+    def __init__(self, embeddingnet):
+        super(Tripletnet, self).__init__()
+        self.embeddingnet = embeddingnet
+
+    def forward(self, x, y, z):
+        embedded_x = self.embeddingnet(x)
+        embedded_y = self.embeddingnet(y)
+        embedded_z = self.embeddingnet(z)
+        dist_a = F.pairwise_distance(embedded_x, embedded_y, 2)
+        dist_b = F.pairwise_distance(embedded_x, embedded_z, 2)
+        return dist_a, dist_b, embedded_x, embedded_y, embedded_z
     
     
 def generate_triplets(gen, 
@@ -84,6 +97,7 @@ def generate_triplets(gen,
     ds_shape   = (n_interpol, n_ancs, n_candidates)
     
     n_can_total = n_ancs * n_candidates
+    z_all = np.random.shuffle(z_all)
     z_all = np.tile(z_all, (n_can_total / len(z_all) + 1, 1))[:n_can_total]
     candidates = np.split(z_all, n_ancs)
     