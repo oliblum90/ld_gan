b37da593bdbd0aa86b9143d329ae29abc062fc4b
Oliver Blum <oblum@compvisgpu02.hci.uni-heidelberg.de>
2017-09-12 11:47:58 +0200

    implementation of pre-suggestion function based on encoded-space

--- ld_gan/train_ops/triplet_enc.py
+++ ld_gan/train_ops/triplet_enc.py
@@ -8,6 +8,7 @@ from ld_gan.utils.model_handler import apply_model, apply_models
 from ld_gan.data_proc.transformer import np_to_tensor, tensor_to_np
 import torch.nn.functional as F
 from sklearn.utils import shuffle
+from sklearn.metrics.pairwise import pairwise_distances
 
 
     
@@ -19,13 +20,14 @@ class TripletEnc:
                  dis,
                  imgs,
                  lr,
-                 margin,
-                 p,
                  n_interpol, 
-                 n_candidates,
+                 n_pos = 10,
+                 n_neg = 10,
+                 quantile_pos = 0.05,
+                 quantile_neg = 0.3,
                  mode = 'min'):
         
-        self.criterion = nn.TripletMarginLoss(margin=margin, p=p)
+        self.criterion = nn.TripletMarginLoss(margin=0.2, p=2)
         self.criterion.cuda()
         
         self.imgs = imgs
@@ -36,7 +38,10 @@ class TripletEnc:
         self.tnet = Tripletnet(self.enc)
         
         self.n_interpol = n_interpol
-        self.n_candidates = n_candidates
+        self.n_pos = n_pos
+        self.n_neg = n_neg
+        self.quantile_pos = quantile_pos
+        self.quantile_neg = quantile_neg
         self.mode = mode
         
         self.opt_enc = optim.Adam(self.tnet.parameters(), lr=lr)
@@ -53,7 +58,10 @@ class TripletEnc:
                                               anchors, 
                                               self.imgs, 
                                               self.n_interpol, 
-                                              self.n_candidates,
+                                              n_pos = self.n_pos,
+                                              n_neg = self.n_neg,
+                                              quantile_pos = self.quantile_pos, 
+                                              quantile_neg = self.quantile_pos,
                                               mode = self.mode)
 
         anchors, pos, neg = np_to_tensor(anchors, pos, neg)
@@ -96,37 +104,49 @@ class Tripletnet(nn.Module):
         dist_b = F.pairwise_distance(embedded_x, embedded_z, 2)
         return dist_a, dist_b, embedded_x, embedded_y, embedded_z
     
-    
+            
+from time import time
 def generate_triplets(enc,
                       gen, 
                       dis, 
                       anchors_x, 
-                      x_all,
+                      imgs,
                       n_interpol, 
-                      n_candidates,
-                      mode = 'min'):
+                      mode = 'min',
+                      n_pos = 10, 
+                      n_neg = 10, 
+                      quantile_pos=0.05, 
+                      quantile_neg=0.3):
     
     #######################################
     # 1. prepare z vectors
     #######################################
     
     # map images to latent space
+    x_all = imgs.copy()
     z_all = apply_models(x_all, None, enc)
     anchors = apply_models(anchors_x, None, enc)
     
     n_ancs     = anchors.shape[0]
     n_features = anchors.shape[1]
+    n_candidates = n_pos + n_neg
     zs_shape   = (n_interpol, n_ancs, n_candidates, n_features)
     ds_shape   = (n_interpol, n_ancs, n_candidates)
     
     n_can_total = n_ancs * n_candidates
     x_all, z_all = shuffle(x_all, z_all)
-    z_all = np.tile(z_all, (n_can_total / len(z_all) + 1, 1))[:n_can_total]
-    x_all = np.tile(x_all, (n_can_total / len(x_all) + 1, 1, 1, 1))[:n_can_total]
     
-    candidates = np.split(z_all, n_ancs)
-    candidates_x = np.split(x_all, n_ancs)
-    
+    # select candidates according to enc-space suggestion
+    idxs_pos, idxs_neg = get_enc_space_suggestion(anchors, 
+                                                  z_all,
+                                                  n_pos=n_pos, 
+                                                  n_neg=n_neg, 
+                                                  quantile_pos=quantile_pos, 
+                                                  quantile_neg=quantile_neg)
+    idxs = np.concatenate([idxs_pos, idxs_neg], 1)
+    candidates = z_all[idxs]
+    x_candidates = x_all[idxs]
+        
     zs = np.zeros(zs_shape)
     for i1 in range(n_interpol):
         for i2 in range(n_ancs):
@@ -139,7 +159,7 @@ def generate_triplets(enc,
             
     zs = zs.reshape(-1, n_features)
             
-        
+    
     #######################################
     # 2. get d-score for z vectors
     #######################################
@@ -163,9 +183,41 @@ def generate_triplets(enc,
     #zs_pos = np.array([c[i] for c, i in zip(candidates, idxs_pos)])
     #zs_neg = np.array([c[i] for c, i in zip(candidates, idxs_neg)])
        
-    xs_pos = np.array([c[i] for c, i in zip(candidates_x, idxs_pos)])
-    xs_neg = np.array([c[i] for c, i in zip(candidates_x, idxs_neg)])
+    xs_pos = np.array([c[i] for c, i in zip(x_candidates, idxs_pos)])
+    xs_neg = np.array([c[i] for c, i in zip(x_candidates, idxs_neg)])
         
     return anchors_x, xs_pos, xs_neg
-            
-            
+
+
+
+
+def get_enc_space_suggestion(z_anc, 
+                             z_all, 
+                             n_pos = 10,
+                             n_neg = 10,
+                             quantile_pos = 0.05, 
+                             quantile_neg = 0.3):
+    
+    n_all = len(z_all)
+    n_anc = len(z_anc)
+    n_pre_pos = int(quantile_pos * n_all)
+    n_pre_neg = int((1.-quantile_neg) * n_all)
+        
+    # get distances in encoded space
+    dists = pairwise_distances(z_anc, z_all)
+    idxs_sorted = np.array([d.argsort() for d in dists])
+    
+    # get indexes fullfilling quantile constraints
+    idxs_pos = idxs_sorted[:, 1:n_pre_pos]
+    idxs_neg = idxs_sorted[:, -n_pre_neg:]
+    
+    # random selection for suggestion
+    idxs_pos = [i[np.random.randint(0, n_pre_pos-1, n_pos)] for i in idxs_pos]
+    idxs_neg = [i[np.random.randint(0, n_pre_neg-1, n_neg)] for i in idxs_neg]
+    idxs_pos = np.array(idxs_pos)
+    idxs_neg = np.array(idxs_neg)
+    
+    return idxs_pos, idxs_neg
+
+
+
